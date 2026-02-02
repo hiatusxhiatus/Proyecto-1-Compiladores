402d180eab0f30d7b85c8ccdbb60189189a89e33
hiatusxhiatus <dzeas@estudiantec.cr>
2026-02-01 23:39:30 -0600

    Update parser grammar and add test cases
    Modified parser.cup and Parser.java to update grammar rules and parsing tables. Regenerated parser classes and added two new test C files for syntax validation.

---
+++ test/prueba0.c
@@ -0,0 +1,85 @@
+
+world string s2 endl  | sintactico
+world string ss endl 
+world float ff endl 
+world int ii endl 
+
+ gift boolean func2  ¿ boolean b1, int i1 ?   ¡ 
+
+ є !@# endl  endl %^& multilinea
+  э
+
+	local int y endl  |sintactico
+	return 5*1 -d endl  |sintactico
+  !  
+
+gift float mi  ¿ int dif, char otra ? ¡  |sintactico  ? 
+	local int otra endl 
+	| local string str="Hola  endl ~&|! endl & ?  mundo" endl 
+	for ¿ i=20  endl  i>=10  endl  --i ?  | puede dar sintactico
+	 ¡ 
+		loop
+		  	var2 = var2 // 1 endl  
+			var = var - 1 endl  
+		exit when ¿ var2!=12.2 @ Σ ¿12 == ¿ 34*35 ??  ?  endl  
+		end loop endl 
+
+		decide   of
+		¿ x24>=var ?  -> | sintactico
+		 ¡ 
+			show ¿ "entra al if" ?  endl 
+		 !
+		end decide endl 
+		decide   of 
+		true ->  
+		 ¡ 
+			show ¿ var ?  endl 
+		 ! 
+		else ->
+		 ¡ 
+			decide   of  
+				otra == 1 -> ¡ 
+					show ¿ "!|" ?  endl  !
+				otra == 2 -> ¡
+					local int otra = 10 endl 
+					break endl  !
+                else -> ¡ | sintactico
+					otra = -10.5 * -1.9 endl  !
+			end  decide   endl 
+		 !
+		 end  decide   endl 
+	 ! 
+	return 1 endl 
+  ! 
+
+ gift boolean miOtraFun  ¿  ?   ¡ 
+	return -5.6 endl  
+  ! 
+
+ coal navidad ¿  ?  ¡ | sintactico
+	local float miFloat=-0.01 endl  |error sintactico
+	local char miChar='!' endl |error sintactico
+	local char miChar2='!!' endl  |sintactico-semantico
+	local char arr[3][3] = ¡¡1,5,10!,¡2,4,5!,¡20,40,50!! endl 
+	arr[67+ ++i][10] = -0.01 endl |sintactico
+	local boolean b1 endl  
+	local string s1 endl  
+	get ¿ s1 ?  endl  
+	show ¿ b1 ?  endl  
+	show ¿ true ?  endl  
+	show ¿ -6.7 ?  endl 
+
+	local float fl1=56.6 endl  
+	local int in1=--fl1- -14% ++in1 + 7 / 15 endl  |sintactico
+	local float fl2=3.7 ^ fl1+ ¿ 45.6 % 76 ?  endl  
+	
+|comentario 2
+	arr = 10 - arr[67+i][1] * func1  ¿ hola, true, "hola mundo", 4.5, 'a' ?  endl  |sintactico arreglo
+	fl1 = 4.5%miChar^-0.005 endl  
+	miFunc ¿ miFunc ¿  ? ,'a' ?  endl  
+	local boolean bl0 = 6.7 != 8.9 endl  |ok
+	bl0 = true != false endl  |ok
+	local boolean bl1 = in1 >= fl1 ~ false @ Σ ¿ func2 ¿ 3,in1 ?  > 56 ?  endl  
+	
+	return 1+f ¿  ?  endl 
+ ! 
